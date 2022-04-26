8f5538ab45c70d322974eb099073961ae0f6f24d
uchihiro <tohchi4122@gmail.com>
2022-04-26 10:43:50 +0900

    PATCHメソッドの追加
    メモを編集するPATCHメソッドを追加しました。

--- memoapp.rb
+++ memoapp.rb
@@ -27,8 +27,29 @@ get "/memos/new" do
   erb :new
 end
 
+get "/memos/:id/edit" do
+  memo_data = JSON.parse(File.read("db/memo.json"), symbolize_names: true).find { |file| file[:id] == params[:id] }
+  @id = memo_data[:id]
+  @title = memo_data[:title]
+  @contents = memo_data[:contents]
+  erb :edit
+end
+
+patch "/memos/:id" do
+  memo_data = JSON.parse(File.read("db/memo.json"), symbolize_names: true)
+  replaced_memo =  memo_data.each do |memo|
+    if memo[:id] == params[:id]
+      memo[:title] = params[:title]
+      memo[:contents] = params[:contents]
+    end
+  end
+  File.open("db/memo.json", "w") { |file| JSON.dump(replaced_memo, file) }
+  redirect "/memos/#{params[:id]}"
+end
+
 get "/memos/:id" do
   memo_data = JSON.parse(File.read("db/memo.json"), symbolize_names: true).find { |file| file[:id] == params[:id] }
+  @id = memo_data[:id]
   @title = memo_data[:title]
   @contents = memo_data[:contents]
   erb :show
