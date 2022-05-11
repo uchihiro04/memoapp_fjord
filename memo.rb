27c93d30542e77686dd2655d505116399c2e92e7
uchihiro <tohchi4122@gmail.com>
2022-05-11 21:18:10 +0900

    インスタンスメソッドをクラスメソッドに変更。メソッドの削除・統合
    インスタンスメソッドをクラスメソッドに変更しました。readメソッドとconvert_jsonメソッドを削除し、allメソッドに統合しました。

--- memo.rb
+++ memo.rb
@@ -2,49 +2,42 @@
 
 class Memo
   JSON_PATH = 'db/memo.json'
-
-  def self.all
-    File.open(JSON_PATH, 'w') unless FileTest.exist?(JSON_PATH)
-    read.empty? ? [] : convert_json
-  end
-
-  def self.create(memo_params)
-    memos = all
-    memo_params[:id] = SecureRandom.uuid
-    memos << memo_params
-    write(memos)
-  end
-
-  def self.update(memo_params)
-    memos = all
-    memos.each do |memo|
-      if memo[:id] == memo_params[:id]
-        memo[:title] = memo_params[:title]
-        memo[:contents] = memo_params[:contents]
-      end
+  class << self
+    def all
+      File.open(JSON_PATH, 'w') {|file| file << [] } unless FileTest.exist?(JSON_PATH)
+      JSON.parse(File.read(JSON_PATH), symbolize_names: true)
     end
-    Memo.write(memos)
-  end
 
-  def self.delete(memo_params)
-    memos = all
-    memos.delete_if { |memo| memo[:id] == memo_params[:id] }
-    Memo.write(memos)
-  end
+    def create(memo_params)
+      memos = all
+      memo_params[:id] = SecureRandom.uuid
+      memos << memo_params
+      write(memos)
+    end
 
-  def self.read
-    File.read(JSON_PATH)
-  end
+    def update(memo_params)
+      memos = all
+      memos.each do |memo|
+        if memo[:id] == memo_params[:id]
+          memo[:title] = memo_params[:title]
+          memo[:content] = memo_params[:content]
+        end
+      end
+      Memo.write(memos)
+    end
 
-  def self.convert_json
-    JSON.parse(read, symbolize_names: true)
-  end
+    def delete(memo_params)
+      memos = all
+      memos.delete_if { |memo| memo[:id] == memo_params[:id] }
+      Memo.write(memos)
+    end
 
-  def self.write(memos)
-    File.open(JSON_PATH, 'w') { |file| JSON.dump(memos, file) }
-  end
+    def write(memos)
+      File.open(JSON_PATH, 'w') { |file| JSON.dump(memos, file) }
+    end
 
-  def self.find(id)
-    convert_json.find { |file| file[:id] == id[:id] }
+    def find(id)
+      all.find { |file| file[:id] == id[:id] }
+    end
   end
 end
