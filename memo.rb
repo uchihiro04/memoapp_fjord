aad2f58e4bba685777c83ae18f704c09a66e340a
uchihiro <tohchi4122@gmail.com>
2022-05-27 16:43:52 +0900

    SQLインジェクションへの対策を追加
    SQLインジェクションへの対策として、変数が入るSQL文に関してはプレースホルダーを使用しました。

--- memo.rb
+++ memo.rb
@@ -5,26 +5,26 @@ class Memo
   class << self
     def all
       memos = []
-      conn = PG.connect( dbname: 'memoapp' )
-      conn.exec( "SELECT * FROM memos" ) do |result|
-        result.each { |row|  memos << row }
+      conn = PG.connect(dbname: 'memoapp')
+      conn.exec('SELECT * FROM memos') do |result|
+        result.each { |row| memos << row }
       end
       memos
     end
 
     def create(params)
-      conn = PG.connect( dbname: 'memoapp' )
-      conn.exec( "INSERT INTO memos (id, title, content) VALUES ('#{SecureRandom.uuid}', '#{params[:title]}', '#{params[:content]}')" )
+      conn = PG.connect(dbname: 'memoapp')
+      conn.exec_params('INSERT INTO memos (id, title, content) VALUES ($1, $2, $3)', [SecureRandom.uuid, params[:title], params[:content]])
     end
 
     def update(params)
-      conn = PG.connect( dbname: 'memoapp' )
-      conn.exec( "UPDATE memos SET title = '#{params[:title]}', content = '#{params[:content]}' WHERE id = '#{params[:id]}'" )
+      conn = PG.connect(dbname: 'memoapp')
+      conn.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [params[:title], params[:content], params[:id]])
     end
 
     def delete(id)
-      conn = PG.connect( dbname: 'memoapp' )
-      conn.exec( "DELETE FROM memos WHERE id = '#{id}'" )
+      conn = PG.connect(dbname: 'memoapp')
+      conn.exec_params('DELETE FROM memos WHERE id = $1', [id])
     end
 
     def find(id)
