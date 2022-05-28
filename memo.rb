78c294b042216638477ffb476acbb071276525c8
uchihiro <tohchi4122@gmail.com>
2022-05-28 16:14:03 +0900

    DB接続を使いまわせるように修正、idの付与方法を変更
    DB接続を使いまわせるように修正しました。また、メモのidをDB側の付与方法を変更しました。

--- memo.rb
+++ memo.rb
@@ -1,30 +1,29 @@
 # frozen_string_literal: true
 
 class Memo
-  JSON_PATH = 'db/memo.json'
   class << self
     def all
       memos = []
-      conn = PG.connect(dbname: 'memoapp')
-      conn.exec('SELECT * FROM memos') do |result|
+      connect_db.exec('SELECT * FROM memos ORDER BY created_at') do |result|
         result.each { |row| memos << row }
       end
       memos
     end
 
+    def connect_db
+      @connect_db ||= PG.connect(dbname: 'memoapp')
+    end
+
     def create(params)
-      conn = PG.connect(dbname: 'memoapp')
-      conn.exec_params('INSERT INTO memos (id, title, content) VALUES ($1, $2, $3)', [SecureRandom.uuid, params[:title], params[:content]])
+      connect_db.exec_params('INSERT INTO memos (title, content) VALUES ($1, $2)', [params[:title], params[:content]])
     end
 
     def update(params)
-      conn = PG.connect(dbname: 'memoapp')
-      conn.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [params[:title], params[:content], params[:id]])
+      connect_db.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [params[:title], params[:content], params[:id]])
     end
 
     def delete(id)
-      conn = PG.connect(dbname: 'memoapp')
-      conn.exec_params('DELETE FROM memos WHERE id = $1', [id])
+      connect_db.exec_params('DELETE FROM memos WHERE id = $1', [id])
     end
 
     def find(id)
